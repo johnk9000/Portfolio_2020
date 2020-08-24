bfb5b8c60f830811b3c83a3119d499e78e17bfbb
johnk9000 <33733453+johnk9000@users.noreply.github.com>
2020-08-24 00:48:24 -0500

    Should be deployable

---
+++ build/precache-manifest.e3f952a032f9b3a678682b5e93e2958e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a489237740062e4d521a40df93ef758",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdb4744029e5373c3976",
+    "url": "/static/css/main.4c9e9588.chunk.css"
+  },
+  {
+    "revision": "b64ea70d5585a1b55fbc",
+    "url": "/static/js/2.9d4e9969.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d4e9969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb4744029e5373c3976",
+    "url": "/static/js/main.ad25b425.chunk.js"
+  },
+  {
+    "revision": "61087ebedb77b8364c43",
+    "url": "/static/js/runtime-main.932df455.js"
+  }
+]);
